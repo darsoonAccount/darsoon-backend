d2811458bc494fb9e669ac4db617ee1c08160061
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-07-28 14:16:06 -0400

    fix bugs in validator

--- src/validator.ts
+++ src/validator.ts
@@ -7,58 +7,111 @@ interface IdataPatternItem {
 }
 
 export const isValid = ({ data, tableSchema, checkNulls }) => {
-  return Object.keys(data).every((key) => {
+  let message = "";
+  let validity = Object.keys(data).every((key) => {
     if (tableSchema[key] === undefined) {
+      message = `${key} is excesive.`;
       return false;
     }
-    return isDataItemValid(data[key], tableSchema[key], checkNulls);
+    const [dataVildity, validationMessage] = isDataItemValid(
+      data[key],
+      tableSchema[key],
+      checkNulls
+    );
+
+    if (!dataVildity) {
+      message = validationMessage;
+      return false;
+    }
+    return true;
   });
+
+  return [validity, message];
 };
 
-export const isDataItemValid = (data, columnSchema, isCheckingNotNulls) => {
-  if (
-    (isCheckingNotNulls === true &&
-      columnSchema.Null === "NO" &&
-      data === undefined) ||
-    data === null ||
-    data === ""
-  ) {
-    return false;
+export const isDataItemValid = (
+  data,
+  columnSchema,
+  isCheckingNotNulls
+): [boolean, string] => {
+  //check not null
+  if (columnSchema.Null === "NO") {
+    if (isCheckingNotNulls === true) {
+      if (data === undefined || data === null || data === "") {
+        return [false, `${columnSchema.Field} must not be empty.`];
+      }
+    } else {
+      if (data === null || data === "") {
+        return [false, `${columnSchema.Field} must not be empty.`];
+      }
+    }
+  } else {
+    if (data === undefined || data === null) {
+      return [true, ""];
+    }
   }
+
+  // check varchars
   const type = columnSchema.Type;
   if (type.includes("varchar")) {
     let length = Number(type.replace("varchar(", "").replace(")", ""));
-    if (typeof data !== "string" || data.length > length) {
-      return false;
+    if (typeof data !== "string") {
+      return [false, `${columnSchema.Field} must be a string.`];
     }
-
+    if (data.length > length) {
+      return [
+        false,
+        `${columnSchema.Field} must not be longer than ${length} character.`,
+      ];
+    }
     switch (columnSchema.Field) {
       case "firstname":
       case "lasttname":
-        return validator.isAlpha(data);
+        return [
+          validator.isAlpha(data),
+          "firstname and lastname should only have alphabetical characters",
+        ];
         break;
       case "email":
-        return validator.isEmail(data);
+        return [
+          validator.isEmail(data),
+          "Please provide a valid email address.",
+        ];
         break;
       default:
-        return typeof data === "string";
+        return [
+          typeof data === "string",
+          `${columnSchema.Field} must be a string.`,
+        ];
         break;
     }
   }
+
+  //check the rest of things
   switch (true) {
     case type.includes("date"):
-      return validator.isDate(data);
+      return [validator.isDate(data), `${columnSchema.Field} must be a date.`];
       break;
     case type.includes("decimal"):
-      return typeof data === "number";
+      return [
+        typeof data === "number",
+        `${columnSchema.Field} must be a numebr.`,
+      ];
       break;
     case type.includes("int"):
-      return typeof data === "number" && Number.isInteger(data);
+      return [
+        typeof data === "number" && Number.isInteger(data),
+        `${columnSchema.Field} must be a integer number.`,
+      ];
       break;
     case type.includes("boolean"):
-      return typeof data === "boolean";
+      return [
+        typeof data === "boolean",
+        `${columnSchema.Field} value must be either true of false.`,
+      ];
       break;
   }
+  return [false, "Something went wrong in the validation!"];
 };
 
 // export const isAllValidOld = (dataPatternArray: IdataPatternItem[]) => {
