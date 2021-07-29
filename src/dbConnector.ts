1a95b66a4489e7f2ae5d1625cef78ca1aa3c0e26
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-07-29 15:39:54 -0400

    refactor dbConnector

--- src/dbConnector.ts
+++ src/dbConnector.ts
@@ -1,5 +1,3 @@
-import { setEnvironmentData } from "worker_threads";
-
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
@@ -13,39 +11,4 @@ export const connectToDB = async () => {
 
   const con = await mysql.createPool(config);
   return con;
-};
-
-interface IfindInDBParameters {
-  sql: string;
-}
-export const findInDB = async ({ sql }: IfindInDBParameters) => {
-  try {
-    const con = await connectToDB();
-    const [rows, fields] = await con.execute(sql);
-    return [null, rows];
-  } catch (error) {
-    return [error, null];
-  }
-};
-
-export const insertInDB = async ({ sql }: IfindInDBParameters) => {
-  try {
-    const con = await connectToDB();
-    const [excutionResult, fields] = await con.execute(sql);
-    return [null, excutionResult];
-  } catch (error) {
-    return [error, null];
-  }
-};
-
-export const deleteInDB = async ({ sql }: IfindInDBParameters) => {
-  try {
-    const con = await connectToDB();
-    await con.connect();
-    const [rows, fields] = await con.execute(sql);
-    return [null, rows];
-  } catch (error) {
-    console.log(error);
-    return [error, null];
-  }
 };
