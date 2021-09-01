862f00b3aa1e66f8ae8d18426bc62a8b87db644e
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-09-01 16:30:59 -0400

    do not send back password

--- src/handlers/customeHandlers/getUserBy.ts
+++ src/handlers/customeHandlers/getUserBy.ts
@@ -1,22 +1,30 @@
 import { getOneAndSend } from "../getOne";
 import { connectToDB } from "../../db/dbConnector";
+import schema from "../../db/schema.json";
 
 export const getUserByEmail = async (email) => {
-  const sql = `SELECT * FROM users WHERE users.email = '${email}'`;
-  const con = await connectToDB();
-  try {
-    const [rows, fields] = await con.execute(sql);
-    if (rows.length === 0) {
-      return null;
-    }
-    return rows[0];
-  } catch (err) {
-    throw err;
+  
+  const userColumnsExceptPassword = Object.keys(schema.users).filter(
+    (key) => key !== "password"
+    );
+    const sql = `SELECT ${userColumnsExceptPassword} FROM users WHERE users.email = '${email}'`;
+    const con = await connectToDB();
+    try {
+      const [rows, fields] = await con.execute(sql);
+      if (rows.length === 0) {
+        return null;
+      }
+      return rows[0];
+    } catch (err) {
+      throw err;
   }
 };
 
 export const getUserById = async (userId) => {
-  const sql = `SELECT * FROM users WHERE users.userId = '${userId}'`;
+  const userColumnsExceptPassword = Object.keys(schema.users).filter(
+    (key) => key !== "password"
+  );
+  const sql = `SELECT ${userColumnsExceptPassword} FROM users WHERE users.userId = '${userId}'`;
   const con = await connectToDB();
   try {
     const [rows, fields] = await con.execute(sql);
@@ -30,14 +38,34 @@ export const getUserById = async (userId) => {
 };
 
 export const getUserByUsername = async (username) => {
-  const sql = `SELECT * FROM users WHERE users.username = '${username}'`;
+  const userColumnsExceptPassword = Object.keys(schema.users).filter(
+    (key) => key !== "password"
+  );
+  const sql = `SELECT ${userColumnsExceptPassword} FROM users WHERE users.username = '${username}'`;
   const con = await connectToDB();
   try {
     const [rows, fields] = await con.execute(sql);
     if (rows.length === 0) {
       return null;
     }
+    console.log(rows[0]);
+    
     return rows[0];
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getPassword = async (userId) => {
+  const sql = `SELECT password FROM users WHERE users.userId = '${userId}'`;
+  const con = await connectToDB();
+  try {
+    const [rows, fields] = await con.execute(sql);
+    if (rows.length === 0) {
+      return null;
+    }
+    
+    return rows[0].password;
   } catch (err) {
     throw err;
   }
